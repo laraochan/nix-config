bb01e3152027c3cb1e566648d76e6b5be20b52c5
larao <me@larao.dev>
2026-08-24 02:31:02 +0900

    fix: stabilize desktop wallpaper path

--- home/programs/desktoppr.nix
+++ home/programs/desktoppr.nix
@@ -1,9 +1,15 @@
 { lib, pkgs, ... }:
+let
+  wallpaper = builtins.path {
+    path = ../../assets/wallpaper.png;
+    name = "wallpaper.png";
+  };
+in
 {
   programs.desktoppr = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
     enable = true;
     settings = {
-      picture = ../../../assets/wallpaper.png;
+      picture = wallpaper;
       scale = "fill";
     };
   };
